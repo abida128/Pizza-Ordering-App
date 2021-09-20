308f980fef0407227e9c95d39303622eadfed12c
unknown <abida.nizam11@gmail.com>
2021-09-20 16:06:20 +0500

    Live Pages

--- src/App.js
+++ src/App.js
@@ -1,17 +1,6 @@
 import React from 'react';
 import Home from './components/Home';
-import Sections from './components/Sections';
-import OrderDetails from './components/OrderDetails';
-import CancelOrder from '././components/CancelOrder';
-import OrderList from './components/OrderList';
 import 'bootstrap/dist/css/bootstrap.min.css';
-import{
-  BrowserRouter as Router,
-  Switch,
-  Route,
-  
-  
-  } from 'react-router-dom';
   
 
 import './App.css';
@@ -20,16 +9,7 @@ import './App.css';
 function App() {
   
   return (
-    <Router>
-      <Switch>
-        <Route exact path={'/home'} component={Home}/>
-        <Route exact path={'/sections'} component={Sections}/>
-        <Route exact path={'/orderDetails'} component={OrderDetails}/>
-        <Route exact path={'/cancelOrder'} component={CancelOrder}/>
-        <Route exact path={'/orderList'} component={OrderList}/>
-
-      </Switch>
-    </Router>
+    <Home />
   
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -2,6 +2,16 @@
 import React from 'react';
 import Images from './Classic.png';
 import { useHistory } from "react-router-dom";
+import{
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    } from 'react-router-dom';
+    
+import Sections from './Sections';
+import OrderDetails from './OrderDetails';
+import CancelOrder from './CancelOrder';
+import OrderList from './OrderList';
 
 
 import Button from 'react-bootstrap/Button';
@@ -15,6 +25,17 @@ function handleClick(e, history) {
 const Home = () => {
     const history = useHistory();
     return (
+        <>
+        <Router>
+      <Switch>
+        <Route exact path={'/home'} component={Home}/>
+        <Route exact path={'/sections'} component={Sections}/>
+        <Route exact path={'/orderDetails'} component={OrderDetails}/>
+        <Route exact path={'/cancelOrder'} component={CancelOrder}/>
+        <Route exact path={'/orderList'} component={OrderList}/>
+
+      </Switch>
+    </Router>
         <div className="container">
             <div className="header">
                 {/* ths is  main header */}
@@ -29,11 +50,12 @@ const Home = () => {
                 <h4>Read to order for your favorite pizza! </h4>
             </div>
             <div className="btn">
-                {/* this is the start button to move to the next page */}
+                {/* this is the start button to move to the  buildnext page */}
                 <Button onClick={e => handleClick(e,history)} >Start Here</Button>
             </div>
 
         </div>
+        </>
     );
 }
 
