4244daabc1fd8812dd4811562b7601a0a2046588
Abida Nizam <83155476+abida128@users.noreply.github.com>
2023-01-09 13:05:36 +0500

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
+// import React from 'react';
+// import Home from './components/Home';
+// import 'bootstrap/dist/css/bootstrap.min.css';
+  
+
+// import './App.css';
+
+
+// function App() {
+  
+//   return (
+//     <Home />
+  
+//   );
+// }
+
+// export default App;
+
+// new changing
+
 import React from 'react';
-import Home from './components/Home';
-import 'bootstrap/dist/css/bootstrap.min.css';
-  
-
-import './App.css';
-
-
-function App() {
-  
-  return (
-    <Home />
-  
-  );
-}
-
-export default App;
+ import { BrowserRouter } from "react-router-dom";
+ import Home from './components/Home';
+ import 'bootstrap/dist/css/bootstrap.min.css';
+ import './App.css';
+ function App() { 
+   return (
+     <BrowserRouter>
+       <Home />
+     </BrowserRouter>
+   );
+ }
+ export default App;
